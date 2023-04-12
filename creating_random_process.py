4cefc4d75d03e448e86c1f74c076a232edfea930
HabiburRahman1 <habib.usa2014@gmail.com>
2023-04-12 16:41:23 +0900

    Creating random data

---
+++ creating_random_process.py
@@ -0,0 +1,10 @@
+# create random process data and save to csv
+import random
+
+
+
+# create random process data and save to csv
+# first is process id
+# second is arrival time
+# third is burst time
+
